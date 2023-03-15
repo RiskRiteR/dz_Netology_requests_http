a0773300dba49ce14aba863de125cb3a16684e85
vladimir kazimir <kazimir.vm@gmail.com>
2023-03-16 00:38:24 +0300

    Task №2, good ver

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 
 import requests
-import yadisk
+
 
 def smartest_hero(list_name_hero: list):
     api_ = requests.get('https://akabab.github.io/superhero-api/api/all.json')
@@ -11,8 +11,37 @@ def smartest_hero(list_name_hero: list):
 
 
 smartest_hero(['Hulk', 'Captain America', 'Thanos'])
+print()
 
 
-y = yadisk.YaDisk(token='y0_AgAAAAAyrFsgAADLWwAAAADeh_uL9UEyoGJsSaG69cZeW1uXiv3MGhs')
-y.upload('file_to_ya_disk/file_1.txt', 'test/file_1.txt')
+class YaUploader:
+    def __init__(self, token: str):
+        self.token = token
+
+    def get_upload_link(self, disk_file_path):
+        upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'OAuth {}'.format(self.token)
+        }
+        params = {"path": disk_file_path, "overwrite": "true"}
+        response = requests.get(upload_url, headers=headers, params=params)
+        data = response.json()
+        url_to_load = data.get('href')
+        return url_to_load
+
+    def upload(self, disk_file_path, local_file_path):
+        href = self.get_upload_link(disk_file_path=disk_file_path)
+        response = requests.put(href, data=open(local_file_path, 'rb'))
+        if response.status_code == 201:
+            print("Success")
+
+
+if __name__ == '__main__':
+    local_path_to_file = input('Введите локальный адрес файла ')
+    disk_path_to_file = input('Введите адрес куда загрузит файл ')
+    TOKEN = input('Введите токен: ')
+    uploader = YaUploader(token=TOKEN)
+    uploader.upload(disk_path_to_file, local_path_to_file)
+
 
